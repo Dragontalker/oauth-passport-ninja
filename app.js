999fd608b19de69edd9b40af978b9f0c28836322
Richard <richard.yang.tong@gmail.com>
2021-04-04 16:33:13 -0400

    Imported keys in configuration.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./configs/passport-setup');
 const mongoose = require('mongoose');
+const keys = require('./configs/keys');
 
 const app = express();
 
