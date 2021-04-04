320fb7abfa4c16b1bb9552a91582e06edfa45bdf
Richard <richard.yang.tong@gmail.com>
2021-04-04 15:20:35 -0400

    Added clientID and clientSecret.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+require('dotenv').config();
 
 const app = express();
 
--- configs/passport-setup.js
+++ configs/passport-setup.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20');
 passport.use(
     new GoogleStrategy({
         // options for the google strat
+        clientID: process.env.ClientID,
+        clientSecret: process.env.ClientSecret
     }), () => {
         // passport callback function
     }
