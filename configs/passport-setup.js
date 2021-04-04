49ef2aaecba1d83cd7956eb51dd49cbc95841864
Richard <richard.yang.tong@gmail.com>
2021-04-04 15:37:59 -0400

    Added a redirect URL.

--- configs/passport-setup.js
+++ configs/passport-setup.js
@@ -5,6 +5,7 @@ const keys = require('./keys');
 passport.use(
     new GoogleStrategy({
         // options for the google strat
+        callbackURL: '/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
     }), () => {
