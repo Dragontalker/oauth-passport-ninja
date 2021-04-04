daab16a78561744107b444d2a4078c9931d0b004
Richard <richard.yang.tong@gmail.com>
2021-04-04 16:32:35 -0400

    Imported Mongoose.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./configs/passport-setup');
+const mongoose = require('mongoose');
 
 const app = express();
 
