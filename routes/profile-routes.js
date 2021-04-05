e1bf8d633d477a16773097b74ba022b8bd4fe1e5
Richard <richard.yang.tong@gmail.com>
2021-04-04 20:46:30 -0400

    Added login status check in authCheck middleware.

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
 const authCheck = (req, res, next) => {
-
+    if (!req.user) {
+        // if user is not logged in
+        res.redirect('/auth/login');
+    } else {
+        // if logged in
+        next();
+    }
 };
 
 router.get('/', (req, res) => {
