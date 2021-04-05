1427af965111bde2e18458e848116cafbc41f10f
Richard <richard.yang.tong@gmail.com>
2021-04-04 20:40:01 -0400

    Exported the Express router.

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -3,3 +3,5 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     res.send('you are logged in, this is your profile -' + req.user.userName);
 });
+
+module.exports = router;
