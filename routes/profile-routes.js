5f008100dc6fb58fac61fadefccce0e95b34af23
Richard <richard.yang.tong@gmail.com>
2021-04-04 20:43:48 -0400

    Created a new middleware to check authentication.

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+const authCheck = (req, res, next) => {
+
+};
+
 router.get('/', (req, res) => {
     res.send('you are logged in, this is your profile -' + req.user.userName);
 });
